827d945d0a17d0b009fc9c035b6f87560556e0f7
SalsabilaZaman <salsabilazamn@gmail.com>
2025-07-01 22:06:25 +0600

    refactor: remove unused AppController and AppService; add Auth, User, and Bookmark modules

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,8 @@
+import { Controller } from "@nestjs/common";
+
+@Controller({}) 
+
+export class AuthController {
+  // This controller will handle authentication-related routes
+  // For example, login, registration, etc.
+}
