e58e5ad222067c0dc72706e3265265f3bf354db8
SalsabilaZaman <salsabilazamn@gmail.com>
2025-07-04 11:52:22 +0600

    add authentication module with signup and signin endpoints; integrate Prisma for database management

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,21 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
 
-@Controller({}) 
-
+@Controller("auth") 
 export class AuthController {
-  // This controller will handle authentication-related routes
-  // For example, login, registration, etc.
-}
+  constructor(private authService:AuthService) {
+
+  }
+  @Post("signup")
+  signup() {
+    // return "I am signup";
+    return this.authService.signup();
+  }
+
+  @Post("signin")
+  signin() {
+    // return "I am signin";
+    return this.authService.login();
+  }
+
+} 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,4 +3,11 @@ import { Injectable } from "@nestjs/common";
 @Injectable()
 export class AuthService{
 
+    login(){
+        return {msg:"Hi login"};
+    }
+    signup(){
+        return {msg:"Hi signup"};
+    }
+
 }
