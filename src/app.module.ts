700752614ec303978b706a39ad9db69182f671b3
SalsabilaZaman <salsabilazamn@gmail.com>
2025-07-07 20:34:49 +0600

    update Prisma1Service to use ConfigService for database URL

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { Prisma1Module } from './prisma1/prisma1.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, Prisma1Module],
-})
+  imports: [AuthModule, UserModule, BookmarkModule, Prisma1Module,ConfigModule.forRoot({
+    isGlobal: true})
+  ]})  
 export class AppModule {}
